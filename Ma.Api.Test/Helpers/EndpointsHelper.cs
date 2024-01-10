94ab914cad7dbacb87bb7b8234965b4ed3fd9b72
macwdo <danilo.macedofernandes@hotmail.com>
2024-01-10 18:59:32 -0300

    configure WebApplicationFixture.cs , creating some tests and installing refit

---
+++ Ma.Api.Test/Helpers/EndpointsHelper.cs
@@ -0,0 +1,9 @@
+namespace Ma.Api.Test.Helpers;
+
+public static class EndpointsHelper
+{
+    public const string RegistriesEndpoint = "/api/v1/registries";
+    public const string LawsuitsEndpoint = "/api/v1/lawsuits";
+    public const string LawyersEndpoint = "/api/v1/lawyers";
+
+}
